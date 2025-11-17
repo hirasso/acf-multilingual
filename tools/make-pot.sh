5a054ec7883e6b519ec16b2fee25712404533fb2
Rasso Hilber <mail@rassohilber.com>
2025-11-17 10:15:33 +0100

    Optimize make-pot script to not update the creation date header each time

--- tools/make-pot.sh
+++ tools/make-pot.sh
@@ -2,4 +2,5 @@
 
 wp i18n make-pot . languages/acfml.pot \
   --include="src,acf-multilingual.php,api.php" \
-  --slug="acf-multilingual"
+  --slug="acf-multilingual" \
+  --headers='{"Report-Msgid-Bugs-To":"https://github.com/hirasso/acf-multilingual/","POT-Creation-Date":""}'
