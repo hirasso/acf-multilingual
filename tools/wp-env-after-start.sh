49559189e0f0b301bda90eabf5ce2178b9a3e156
Rasso Hilber <mail@rassohilber.com>
2025-12-28 12:17:44 +0100

    Add docs to the wp-env start script

--- tools/wp-env-after-start.sh
+++ tools/wp-env-after-start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# Activate the theme
 wp-env run cli wp theme activate twentytwentyfive
+# Rename the plugin config file
 wp-env run cli bash -c 'cp /var/www/html/wp-content/plugins/acf-multilingual/acfml.config.sample.json /var/www/html/wp-content/themes/twentytwentyfive/acfml.config.json'
+# Delete the default 'WP_LANG_DIR' define
 wp-env run cli sed -i "/define( 'WP_LANG_DIR'/d" /wordpress-phpunit/wp-tests-config.php
